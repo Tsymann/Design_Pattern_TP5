69b5c24658ded4c8b48231e1a0b3a59ef8bd2322
MatthisBrault <matthis.brault@my-digital-school.org>
2021-02-03 16:56:12 +0100

    tp5 command

--- model/GlobalStock.java
+++ model/GlobalStock.java
@@ -10,31 +10,28 @@ public class GlobalStock {
 	private List<Stock> stocks = new ArrayList<Stock>();
 	private Invoker invoker = new Invoker();
 
-
 	public List<Stock> getStocks() {
 		return stocks;
-	}	
-	
-	 public void executeCommand(Command command) throws CloneNotSupportedException {
-		    if (command.execute()) {
-		      this.invoker.push(command);
-		    }
-		  }
-	 
+	}
 
-	 public void showHistory() {
+	public void executeCommand(Command command) throws CloneNotSupportedException {
+		if (command.execute()) {
+			this.invoker.push(command);
+		}
+	}
+
+	public void showHistory() {
 		this.invoker.showHistory();
 	}
-	
-	
+
 	public void printStock() {
-		if(!stocks.isEmpty()) {
-			for(Stock stock : stocks) {
+		if (!stocks.isEmpty()) {
+			for (Stock stock : stocks) {
 				System.out.println(stock.toString());
 			}
 		} else {
-			System.out.println("il n'y à pas de stock");
+			System.out.println("il n'y a pas de stock");
 		}
-	} 
-	
+	}
+
 }
